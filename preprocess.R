293fa0f857b4bb7aad908c2c813c226d41a77b65
Gabriel Carvalho <gabriel_carvalho@usp.br>
2024-12-06 00:10:15 -0300

    shorten data universe

--- preprocess.R
+++ preprocess.R
@@ -1,122 +1,83 @@
-
-### RNAseq
-## Jovem
-c = 3
-for (i in rnaseq.dirs) {
-    tmp <- read.delim(paste0('./data/rnaseq/jovem/',  i, '/', list.files(paste0('data/rnaseq/jovem/', i), pattern = '.tsv')), skip = 1)[-c(1:4), ]
-    tmp <- tmp[, 4]
-    raw_counts <- cbind(raw_counts, tmp)
-    colnames(raw_counts)[c] <- i
-    c = c+1
-    print(c)
+# Define helper function for RNAseq data processing
+load_rnaseq_data <- function(base_path, category) {
+  category_path <- file.path(base_path, "rnaseq", category)
+  rnaseq_dirs <- list.files(category_path)
+  
+  if (length(rnaseq_dirs) == 0) {
+    stop(paste("No directories found in", category_path))
+  }
+  
+  # Read and combine RNAseq data
+  raw_counts <- lapply(rnaseq_dirs, function(dir) {
+    file_path <- list.files(file.path(category_path, dir), pattern = '\\.tsv$', full.names = TRUE)
+    if (length(file_path) == 0) {
+      stop(paste("No TSV files found in", file.path(category_path, dir)))
+    }
+    tmp <- read.delim(file_path, skip = 1)[-c(1:4), c(1, 4)]
+    colnames(tmp) <- c("gene_id", dir)
+    tmp
+  })
+  
+  # Merge data frames by gene_id
+  raw_counts <- Reduce(function(x, y) merge(x, y, by = "gene_id", all = TRUE), raw_counts)
+  
+  # Convert to matrix and set rownames
+  rownames(raw_counts) <- raw_counts$gene_id
+  as.matrix(raw_counts[, -1])
 }
-rm(tmp)
-head(raw_counts)
-dim(raw_counts)
-rownames(raw_counts) <- raw_counts$gene_id
-raw_counts <- as.matrix(raw_counts[, -1])
-class(raw_counts)
-is.numeric(raw_counts)
-raw_counts[1:4, 1:4]
 
-saveRDS(raw_counts, file = 'data/processed_data/RNASEQ_JOVEM.rds')
-## Nao jovem
-c = 3
-for (i in rnaseq.dirs) {
-    tmp <- read.delim(paste0('./data/rnaseq/naojovem/',  i, '/', list.files(paste0('data/rnaseq/naojovem/', i), pattern = '.tsv')), skip = 1)[-c(1:4), ]
-    tmp <- tmp[, 4]
-    raw_counts <- cbind(raw_counts, tmp)
-    colnames(raw_counts)[c] <- i
-    c = c+1
-    print(c)
+# Define helper function for MAF data processing
+load_maf_data <- function(base_path, category) {
+  category_path <- file.path(base_path, "maf", category)
+  maf_dirs <- list.files(category_path)
+  
+  if (length(maf_dirs) == 0) {
+    stop(paste("No directories found in", category_path))
+  }
+  
+  # Read and combine MAF data
+  maf <- do.call(rbind, lapply(maf_dirs, function(dir) {
+    file_path <- list.files(file.path(category_path, dir), pattern = 'maf.gz$', full.names = TRUE)
+    if (length(file_path) == 0) {
+      stop(paste("No MAF files found in", file.path(category_path, dir)))
+    }
+    tmp <- read.delim(file_path, skip = 7)[, c(1, 9, 16)]
+    colnames(tmp) <- c("Hugo_Symbol", "Tumor_Sample_Barcode", "Variant_Classification")
+    tmp
+  }))
+  
+  maf
 }
-rm(tmp)
-head(raw_counts)
-dim(raw_counts)
-rownames(raw_counts) <- raw_counts$gene_id
-raw_counts <- as.matrix(raw_counts[, -1])
-class(raw_counts)
-is.numeric(raw_counts)
-raw_counts[1:4, 1:4]
 
-saveRDS(raw_counts, file = 'data/processed_data/RNASEQ_JOVEM.rds')
-
-
-### MAF
-## Jovem
-maf <- read.delim(paste0('./data/maf/',  maf.dirs[1], '/', list.files(paste0('data/maf/', maf.dirs[1]), pattern = 'maf.gz')), skip = 7)
-head(maf)
-colnames(maf)
-maf <- maf[, c(1,9,16)]
-head(maf)
-
-for (i in maf.dirs[-1]) {
-    tmp <- read.delim(paste0('./data/maf/',  i, '/', list.files(paste0('data/maf/', i), pattern = 'maf.gz')), skip = 7)
-    tmp <- tmp[, c(1,9,16)]
-    maf <- rbind(maf, tmp)
-    print(i)
+# Main function to process data
+process_data <- function(base_path, categories, data_type) {
+  processed_data <- lapply(categories, function(category) {
+    if (data_type == "rnaseq") {
+      load_rnaseq_data(base_path, category)
+    } else if (data_type == "maf") {
+      load_maf_data(base_path, category)
+    }
+  })
+  
+  # Save processed data for each category
+  names(processed_data) <- categories
+  for (category in categories) {
+    saveRDS(processed_data[[category]], file = file.path(base_path, "processed_data", paste0("TCGA_UCS_", toupper(data_type), "_", toupper(category), ".rds")))
+  }
+  
+  processed_data
 }
-head(maf)
-barplot(table(maf$Tumor_Sample_Barcode)[order(table(maf$Tumor_Sample_Barcode))])
-maf$Tumor_Sample_Barcode[grep('POLE', maf$Hugo_Symbol)]
 
-table(maf$Variant_Classification)
-table(maf$Tumor_Sample_Barcode, maf$Hugo_Symbol)
+# Paths and categories
+base_path <- "data"
+categories <- c("jovem", "naojovem")
 
-muts <- c('Frame_Shift_Del', 'Frame_Shift_Ins', 'Missense_Mutation', 'Nonsense_Mutation')
+# Process RNAseq data
+rnaseq_data <- process_data(base_path, categories, "rnaseq")
 
-saveRDS(maf, file = './data/processed_data/TCGA_UCS_MAF.rds')
+# Process MAF data
+maf_data <- process_data(base_path, categories, "maf")
 
-maf <- maf[which(maf$Variant_Classification %in% muts), ]
-table(maf$Variant_Classification)
-
-saveRDS(maf, file = './data/processed_data/TCGA_UCS_MAF_filterd.rds')
-raw_counts[1:4,1:4]
-head(maf)
-
-
-#### Integrar dados
-
-manifest <- read.delim('manifests/gdc_manifest.2024-11-29.084914.txt.map2submitterID')
-head(manifest)
-manifest$cases.0.samples.0.submitter_id[which(manifest$id == '0f638551-a5b7-4765-841e-23ae1424f14c')]
-
-rownames(manifest) <- manifest$id
-manifest <- manifest[colnames(raw_counts), ]
-all(colnames(raw_counts) == manifest$id)
-
-colnames(raw_counts) <- manifest$cases.0.samples.0.submitter_id
-
-raw_counts[1:4, 1:4]
-
-head(maf)
-maf$barcode <- stringr::str_sub(maf$Tumor_Sample_Barcode, 1,16)
-
-maf$Hugo_Symbol <- as.character(as.factor(maf$Hugo_Symbol))
-names(table(maf$Hugo_Symbol)[order(table(maf$Hugo_Symbol), decreasing = TRUE)][1:10])
-
-maf_f <- maf[which(maf$Hugo_Symbol %in% names(table(maf$Hugo_Symbol)[order(table(maf$Hugo_Symbol), decreasing = TRUE)][1:10])), ]
-maf_table <- table(maf_f$barcode, maf_f$Hugo_Symbol)
-
-head(maf_table)
-
-dim(maf_table)
-dim(raw_counts)
-
-
-intersect(rownames(maf_table), colnames(raw_counts))
-mis <- colnames(raw_counts)[which(!colnames(raw_counts) %in% rownames(maf_table))]
-
-dim(maf_table)
-maf_table <- rbind(maf_table, rep(0, ncol(maf_table)))
-rownames(maf_table)[c(56,57)] <- mis
-maf_table
-
-maf_table <- maf_table[colnames(raw_counts), ]
-
-all(rownames(maf_table) == colnames(raw()))
-maf_table[maf_table > 1] <- 1
-
-
-saveRDS(raw_counts, file = 'data/processed_data/TCGA_UCS_RNASEQ_MATCHED.rds')
-saveRDS(maf_table, file = 'data/processed_data/TCGA_UCS_MAF_MATCHED.rds')
+# Optional: Combine RNAseq data for integration
+combined_rnaseq <- do.call(cbind, rnaseq_data)
+saveRDS(combined_rnaseq, file = file.path(base_path, "processed_data", "TCGA_UCS_RNASEQ_COMBINED.rds"))
